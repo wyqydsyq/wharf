06c5facb884ae319f1a9061c3052e7f6d4b1a774
wyqydsyq <pyrokinetiq@gmail.com>
2018-03-09 17:19:30 +1000

    Sexy text

--- src/client/Main.ts
+++ src/client/Main.ts
@@ -1,10 +1,21 @@
 import xs from 'xstream'
-import { pre } from '@cycle/dom'
+import { h1 } from '@cycle/dom'
 
 export const Main = sources => {
-  const wsSource = sources.WS || xs.from([])
+  const wsSource$ = sources.WS || xs.from([])
+  const message$ = wsSource$.map(ev => ev.data).startWith(['🔥'])
+  const DOM$ = message$.map(msg =>
+    h1(
+      {
+        attrs: {
+          style: 'text-shadow: 0px 5px 10px #666; text-align: center'
+        }
+      },
+      [msg]
+    )
+  )
   return {
-    DOM: wsSource.map(ev => ev.data).startWith(pre(['🔥'])),
+    DOM: DOM$,
     WS: xs.of('Hello from the client!')
   }
 }
