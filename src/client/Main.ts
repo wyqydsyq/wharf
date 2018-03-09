8de70025072cd1ec3e2fa6ba70f69ee8003edc1b
pyrokinetiq <pyrokinetiq@gmail.com>
2018-03-09 21:48:20 +1000

    Sexiness

--- src/client/Main.ts
+++ src/client/Main.ts
@@ -1,17 +1,35 @@
 import xs from 'xstream'
-import { pre } from '@cycle/dom'
+import { pre, div } from '@cycle/dom'
 
 export const Main = sources => {
   const wsSource$ = sources.WS || xs.from([])
-  const message$ = wsSource$.map(ev => JSON.parse(ev.data)).startWith('🔥')
+  const message$ = wsSource$.map(ev => JSON.parse(ev.data)).startWith(null)
   const DOM$ = message$.map(msg => {
-    return pre(
+    return div(
       {
         attrs: {
-          style: 'max-width: 100%;'
+          style:
+            'display: flex; justify-content: center; align-items: center; height: 100%;'
         }
       },
-      [JSON.stringify(msg, null, '\t')]
+      [
+        pre(
+          {
+            attrs: {
+              style:
+                'flex: 0 0 50%; background: #eee; padding: 1rem; border-radius: 5px; box-shadow: 0 10px 200px rgba(0, 0, 0, .5);'
+            }
+          },
+          [
+            msg
+              ? 'Connected Clients: \n' + JSON.stringify(msg, null, '\t')
+              : div(
+                  { attrs: { style: 'text-align: center; font-size: 4em' } },
+                  ['🔥']
+                )
+          ]
+        )
+      ]
     )
   })
   return {
